2437d5fa645ac9f4728415e0f05cf449a5f06971
saba-rrz <101900561+saba-rrz@users.noreply.github.com>
2022-11-03 09:54:40 -0400

    Added Gun
    Added Scripts for Gun
    Added Tree model

---
+++ FPS GE Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,44 @@
+
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+
+
+    public float damage;
+    public float range;
+    public Camera fpsCam;
+    // public ParticleSystem muzzleFlash;
+    
+    private void Start()
+    {
+        damage = 10f;
+        range = 100f;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+
+        void Shoot()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+                Target target = hit.transform.GetComponent<Target>();
+
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+            }
+
+        }
+    }
+}
---
+++ FPS GE Game/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;   
+        if (health<= 0f)
+        {
+            Die();
+
+        }
+        
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
