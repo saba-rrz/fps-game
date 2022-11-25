04634d41b9c3843e2a99e36bc8517580713072a5
saba-rrz <101900561+saba-rrz@users.noreply.github.com>
2022-11-24 20:29:55 -0500

    reloading and UI
    + added the reloading script
    + added the UI
    + bullet Update
    + weapon switching

--- FPS GE Game/Assets/Scripts/Gun.cs
+++ FPS GE Game/Assets/Scripts/Gun.cs
@@ -1,25 +1,86 @@
+using JetBrains.Annotations;
 using Palmmedia.ReportGenerator.Core.Reporting.Builders;
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 100f;
+    [SerializeField] private float fireRate = 15f;
+    
 
+    [SerializeField] private int maxBulets = 3;
+    [SerializeField] private int bullets;
+    [SerializeField] private float reloadTime = 1f;
+    private bool IsReloading=false;
+
+
+    //public Animator animator;
+
+    private float nextTimeToFire = 0f;
+   
+
+    [SerializeField] private Sprite icon;
+    [SerializeField] private WeaponUI weaponUI;
     public Camera playerCamera;
     public ParticleSystem muzzleFlash;
+
+    private void Start()
+    {
+        bullets = maxBulets;
+    }
+    private void OnEnable()
+    {
+        IsReloading = false;
+        //animator.SetBool("Reloading", false);
+    }
     private void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (IsReloading)
         {
-            Shoot();
+            return;
+
         }
+        if (bullets <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        
+        if (Input.GetButtonDown("Fire1")&& Time.time>=nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+           
+            
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        IsReloading = true;
+        //animator.SetBool("Reloading",true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+       // animator.SetBool("Realoding",false);
+        yield return new WaitForSeconds(1f);
+
+
+        bullets = maxBulets;
+        IsReloading = false;
+
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
     void Shoot()
     {
+       
+
+
         muzzleFlash.Play();
+        bullets--;
         
         RaycastHit raycastHit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out raycastHit, range))
@@ -32,6 +93,9 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+            //update damage
         }
+        
     }
+   
 }
---
+++ FPS GE Game/Assets/Scripts/WeaponUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WeaponUI : MonoBehaviour
+{
+   
+    public int bullets = 3;
+    public GameObject[] bulletArray;
+
+    private void Update()
+    {
+        DisplayBullets(bullets);
+    }
+    private void DisplayBullets(int index)
+    {
+        for(int i = 0; i < bulletArray.Length; i++)
+        {
+            if (i < bullets)
+            {
+                bulletArray[i].SetActive(true);
+            }
+            else
+            {
+                bulletArray[i].SetActive(false);
+            }
+        }
+    }
+
+}
